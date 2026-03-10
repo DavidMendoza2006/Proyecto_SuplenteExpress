f16fe980e7d586c9cf1471405b7a130b27c8b8fe
david <davmenmor2@alu.edu.gva.es>
2026-03-10 14:28:33 +0100

    correccionPaginasPHP

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DA1 Premier Luxury Car</title>
+    <title>Index - DA1MOTORS</title>
     <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="./bootstrap-icons/bootstrap-icons.css">
